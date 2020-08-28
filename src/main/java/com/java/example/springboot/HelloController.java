0f2ac28deac6328f226d1b2fdabe4f0bccacf07b
vijay <vijay@KUNDAM-DESKTOP>
2020-08-28 17:10:42 +0530

    FIRST AWS CICD SUCCESS

--- src/main/java/com/java/example/springboot/HelloController.java
+++ src/main/java/com/java/example/springboot/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Welomce to TeST AWS";
+        return "Welcome to MY First CICD IMPLEMENTATION USING AWS SERVICES";
     }
 }
