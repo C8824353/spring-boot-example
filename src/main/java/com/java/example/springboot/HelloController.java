80894a6499bc598cf307ba76098d9d369cc59d63
vijay <vijay@KUNDAM-DESKTOP>
2020-08-28 17:31:44 +0530

    MODIFIED THE MESSAGE

--- src/main/java/com/java/example/springboot/HelloController.java
+++ src/main/java/com/java/example/springboot/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Welcome to MY First CICD IMPLEMENTATION USING AWS SERVICES";
+        return "Welcome to MY First CICD IMPLEMENTATION USING AWS SERVICES=========================";
     }
 }
